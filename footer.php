ef62028e5bf9405d5b9aa784a7beeb16dc9884bb
Riccardo Carlesso <palladiusbonton@gmail.com>
2024-10-08 13:27:59 +0200

    1.2.4 adding PHP ENV to explain the local env.

--- footer.php
+++ footer.php
@@ -3,9 +3,12 @@
     <?php
     # PHP 5.3 syntax. In 7 its more elegant: $name = $_GET['name'] ?? 'john doe';
     $appname = getenv('APP_NAME')  ?: '_APP_NAME_SCONOSCIUTA_'; # https://stackoverflow.com/questions/5972516/best-way-to-give-a-variable-a-default-value-simulate-perl
+    $php_env = getenv('PHP_ENV')  ?: 'testing-I-guess'; # https://stackoverflow.com/questions/5972516/best-way-to-give-a-variable-a-default-value-simulate-perl
+
     #$app_version = getenv('APP_VERSION')  ?: '0.0.?';
     $app_version = file_get_contents('VERSION');
     echo "Welcome to <b>$appname</b> v<b>$app_version</b>!";
     # 'version' => env('APP_VERSION', '1.0.0'),
+    echo "<tt>PHP_ENV=$php_env</tt>"
     ?>
 </div>
