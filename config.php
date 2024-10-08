da1cb390952fecb95ef7f9fcf1caf065fcc241a3
Riccardo Carlesso <palladiusbonton@gmail.com>
2024-10-08 16:57:02 +0200

    1.6.0 proviamo con underscore

--- config.php
+++ config.php
@@ -9,7 +9,8 @@ try {
     $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Errore di connessione al db <b>$db_name</b> sull'host $db_host: " . $e->getMessage());
+    // Riccardo toglimi!!!
+    die("[DEBUG DEPLOY db_pass='$db_pass'] Errore di connessione al db <b>$db_name</b> sull'host $db_host: " . $e->getMessage());
 }
 
 session_start();
