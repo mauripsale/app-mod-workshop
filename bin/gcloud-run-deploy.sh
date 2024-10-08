3f1fca1a1674f55ca01347d18e58076612ab29a3
Riccardo Carlesso <palladiusbonton@gmail.com>
2024-10-08 15:57:48 +0200

    1.6.0 integrating SM

--- bin/gcloud-run-deploy.sh
+++ bin/gcloud-run-deploy.sh
@@ -10,10 +10,11 @@ set -euo pipefail
 # ERROR: (gcloud.run.deploy) spec.template.spec.containers[0].env: The following reserved env names were provided: PORT. These values are automatically set by the system.
 
 gcloud --project "$PROJECT_ID" run deploy \
-    php-amarcord \
+    php-amarcord-bin \
     --source . \
     --port 80 \
-    --set-env-vars DB_PASS="$DB_PASS" \
+    --set-secrets DB_PASS="" \
+#    --set-env-vars DB_PASS="$DB_PASS" \
     --set-env-vars DB_USER="$DB_USER" \
     --set-env-vars DB_HOST="$DB_HOST" \
     --set-env-vars DB_NAME="$DB_NAME" \
