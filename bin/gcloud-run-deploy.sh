6ed79ef6f58c8db72245b867822efce17c0c6231
Riccardo Carlesso <palladiusbonton@gmail.com>
2024-10-08 16:03:27 +0200

    1.6.0 trying double dollar

--- bin/gcloud-run-deploy.sh
+++ bin/gcloud-run-deploy.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+DEPLOY_VER='1.1'
+
+########################
+# CHANGELOG
+########################
+#
+# 8oct24  v1.1 Adding DB_PASS from Secret Manager
+# 6oct24  v1.0 All of it.
+#
+########################
+
+
 . .env
 
 set -euo pipefail
@@ -8,13 +20,16 @@ set -euo pipefail
 
 #    --set-env-vars PORT="80" \
 # ERROR: (gcloud.run.deploy) spec.template.spec.containers[0].env: The following reserved env names were provided: PORT. These values are automatically set by the system.
+SECRET_MANAGER_PERMAURL_DB_PASS='projects/839850161816/secrets/php-amarcord-db-pass'
 
+# For Riccardo
+#    --set-env-vars DB_PASS="$DB_PASS" \
+## DB_PASS: projects/839850161816/secrets/php-amarcord-db-pass
 gcloud --project "$PROJECT_ID" run deploy \
     php-amarcord-bin \
     --source . \
     --port 80 \
-    --set-secrets DB_PASS="" \
-#    --set-env-vars DB_PASS="$DB_PASS" \
+    --set-secrets DB_PASS="$SECRET_MANAGER_PERMAURL_DB_PASS" \
     --set-env-vars DB_USER="$DB_USER" \
     --set-env-vars DB_HOST="$DB_HOST" \
     --set-env-vars DB_NAME="$DB_NAME" \
@@ -22,3 +37,6 @@ gcloud --project "$PROJECT_ID" run deploy \
     --set-env-vars APP_VERSION="$APP_VERSION" \
     --region europe-west8 \
     --allow-unauthenticated
+
+# Secret mounting:
+#--update-secrets=/secrets/api/key=mysecret:latest,ENV=othersecret:1
