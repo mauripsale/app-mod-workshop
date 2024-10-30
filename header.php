49f68a7c8dbeeaa9ea5aecafb0385c9070ae2d68
Riccardo Carlesso <palladiusbonton@gmail.com>
2024-10-30 18:23:28 +0100

    1.10.3 oops forgot CDN in previous push

--- header.php
+++ header.php
@@ -1,13 +1,17 @@
-<div class='center'>
-    [
-        <a href='/'>🏠 Home</a>
-        |
-        <a href='/register.php'>👉 Registrati</a>
-        |
-        <a href='/gemini.php'>♊️ Gemini (broken)</a>
-        |
-        <a href='/upload.php'>📸 Upload Pic</a>
-        |
-        <a href='/logout.php'>↪️ Logout</a>
-    ]
-</div>
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class='center'>
+        [
+            <a href='/'>🏠 Home</a>
+            |
+            <a href='/register.php'>👉 Registrati</a>
+            |
+            <a href='/gemini.php'>♊️ Gemini (broken)</a>
+            |
+            <a href='/upload.php'>📸 Upload Pic</a>
+            |
+            <a href='/logout.php'>↪️ Logout</a>
+        ]
+    </div>
